53cedb443b75007669d517e20a258bbacc7d23e3
julien yassef <juyassef@gmail.com>
2024-07-01 19:05:42 +0200

    create et ajout logonaimation

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,47 +1,57 @@
-// CSS
+// App.jsx
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
-//Reactte
-import { BrowserRouter, Route, Routes, } from 'react-router-dom';
-
-//i18n
+// i18n
 import '../i18n.js';
 
-//Import
+// Import
 import { ThemeProvider } from '../provider/ThemeProvider.jsx';
 
-//Components
+// Components
 import NavBar from '../components/layout/NavBar/NavBar.jsx';
 import Footer from "../components/layout/Footer/Footer.jsx";
+import LogoAnimation from '../components/LogoAnimation/LogoAnimation';
 
-//Page
+// Pages
 import Home from '../page/Home/Home';
 import Error404 from '../page/Error404/Error404';
-import LegalNotice from '../page/LegalNotice/LegalNotice'; 
-import SiteMap from '../page/SiteMap/SiteMap'; 
+import LegalNotice from '../page/LegalNotice/LegalNotice';
+import SiteMap from '../page/SiteMap/SiteMap';
 
-//Data
-import DataProvider from '../provider/DataProvider.jsx'
+// Data
+import DataProvider from '../provider/DataProvider.jsx';
 
 function App() {
+  const [showAnimation, setShowAnimation] = useState(true);
 
+  const handleAnimationEnd = () => {
+    setShowAnimation(false);
+  };
 
   return (
-    <BrowserRouter >
+    <BrowserRouter>
       <ThemeProvider>
         <DataProvider>
-          <NavBar />
-            <Routes>
+          {showAnimation ? (
+            <LogoAnimation onAnimationEnd={handleAnimationEnd} />
+          ) : (
+            <>
+              <NavBar />
+              <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/legal-notice" element={<LegalNotice />} />
                 <Route path="/site-map" element={<SiteMap />} />
                 <Route path="/*" element={<Error404 />} />
-            </Routes>
-          <Footer />
+              </Routes>
+              <Footer />
+            </>
+          )}
         </DataProvider>
       </ThemeProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/LogoAnimation/LogoAnimation.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import './LogoAnimation.scss';
+import circle from '../../../public/assets/pictureAnimation/Ellipse.png'; 
+import letterY from '../../../public/assets/pictureAnimation/lettreY.png';
+import letterJ from '../../../public/assets/pictureAnimation/lettreJ.png';
+
+const LogoAnimation = ({ onAnimationEnd }) => {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            document.querySelector('.logo-animation-container').classList.add('fade-out');
+            setTimeout(() => {
+                onAnimationEnd();
+            }, 1000); 
+        }, 4000); 
+
+        return () => clearTimeout(timer);
+    }, [onAnimationEnd]);
+
+    return (
+        <div className="logo-animation-container">
+            <div className="logo">
+                <img src={letterY} alt="Y" className="letter y" />
+                <img src={letterJ} alt="J" className="letter j" />
+                <div className="circle" style={{ backgroundImage: `url(${circle})` }}></div>
+            </div>
+        </div>
+    );
+};
+
+LogoAnimation.propTypes = {
+    onAnimationEnd: PropTypes.func.isRequired,
+};
+
+export default LogoAnimation;
